52417bd91a1402625f1939c2374fd4d0d69df1b4
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2023-12-21 14:38:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ def Requirements(filename):
 
 setup(
     name="Travel_Package_Prediction",
-    author="Gajender",
+    author="Devika",
     version="0.1",
-    author_email="iamsanju0707@gmail.com",
+    author_email="devika.be17@gmail.com",
     description="All about the Prediction of Travel Package wheather a customer will purchse it or not.",
     packages=find_packages(),
     install_requires=Requirements("requirements.txt")
